10998ce2f41ed43682c3edbd484b3b0dd03fc1da
FedeMontani <132162195+FedeMontani@users.noreply.github.com>
2023-06-09 19:13:58 -0300

    Add files via upload

---
+++ punto_b.m
@@ -0,0 +1,137 @@
+#DATOS
+
+Ka = 0.01;
+ODs = 9;
+Kbd = 0.1;
+KO2^2 = 1.4;
+
+Q_e = [];
+for i = 1:365
+  if i<=31
+    Q_e(i) = 6;
+  elseif 31<i<=59
+    Q_e(i) = 9;
+  elseif 59<i<=90
+    Q_e(i) = 12;
+  elseif 90<i<=120
+    Q_e(i) = 15;
+  elseif 120<i<=151
+    Q_e(i) = 19;
+  elseif 151<i<=181
+    Q_e(i) = 25;
+  elseif 181<i<=212
+    Q_e(i) = 25;
+  elseif 212<i<=243
+    Q_e(i) = 18;
+  elseif 243<i<=273
+    Q_e(i) = 14;
+  elseif 273<i<=304
+    Q_e(i) = 10;
+  elseif 304<i<=334
+    Q_e(i) = 7;
+  elseif i>334
+    Q_e(i) = 6;
+  endif
+endfor
+
+Q_s = [];
+for i = 1:365
+  if i<=31
+    Q_s(i) = 0;
+  elseif 31<i<=59
+    Q_s(i) = 0;
+  elseif 59<i<=90
+    Q_s(i) = 12;
+  elseif 90<i<=120
+    Q_s(i) = 15;
+  elseif 120<i<=151
+    Q_s(i) = 19;
+  elseif 151<i<=181
+    Q_s(i) = 44;
+  elseif 181<i<=212
+    Q_s(i) = 34;
+  elseif 212<i<=243
+    Q_s(i) = 18;
+  elseif 243<i<=273
+    Q_s(i) = 14;
+  elseif 273<i<=304
+    Q_s(i) = 10;
+  elseif 304<i<=334
+    Q_s(i) = 0;
+  elseif i>334
+    Q_s(i) = 0;
+  endif
+endfor
+
+#EJERCICIO A
+
+#Metodo de Euler
+
+u_n+1 = u_n + h*f(u_n,t_n)
+
+dv/dt = Q_e - Q_s
+dOD/dt = (Q_e*C_eOD - Q_s*C_sOD + Ka*V*(ODs - OD) - Kbd*OD^2/(OD^2+KO2^2)*V*DBO)/V
+dDBO/dt = (Q_e*C_eDBO - Q_s*C_sDBO + Ka*V*(ODs - OD) - Kbd*OD^2/(OD^2+KO2^2)*V*DBO)/V
+
+#EJERCICIO B
+
+Volumen = [0; 66; 481; 948];
+Cota = [74; 76; 78; 80];
+
+#Interpolacion por Lagrange
+
+produ_denominadores_delta  = [];
+for i = 1:4
+    denominadores_delta = [];
+    k = 0;
+    for j = 1:4
+            if j != i
+                    k = k+1;
+                      denominadores_delta(k) = (Volumen(i) - Volumen(j));
+            endif
+    endfor
+    produ_denominadores_delta(i) = prod(denominadores_delta(1,:));
+endfor
+
+function L = l(x, Volumen,produ_denominadores_delta, Cota)  #polinomio interpolante
+    delta0 = (x-Volumen(2)).*(x-Volumen(3)).*(x-Volumen(4))/produ_denominadores_delta(1);
+    delta1 = (x-Volumen(1)).*(x-Volumen(3)).*(x-Volumen(4))/produ_denominadores_delta(2);
+    delta2 = (x-Volumen(1)).*(x-Volumen(2)).*(x-Volumen(4))/produ_denominadores_delta(3);
+    delta3 = (x-Volumen(1)).*(x-Volumen(2)).*(x-Volumen(3))/produ_denominadores_delta(4);
+    L = delta0.*Cota(1) + delta1.*Cota(2) + delta2.*Cota(3) + delta3.*Cota(4);
+endfunction
+
+x = 0:25:1000;
+plot(x,l(x, Volumen,produ_denominadores_delta, Cota))
+hold on;
+scatter(Volumen, Cota,10, 'filled')
+hold off;
+xlabel('Volumen [Hm^3]','fontsize',14)
+ylabel('Cota [m]','fontsize',14)
+title('Polinomio Interpolador de Lagrange','fontsize',14,'color','blue')
+grid
+
+#Ajuste cuadratico
+
+fi0 = [1; 1; 1; 1];
+fi1 = [0; 66; 481; 948];
+fi2 = [0; 66^2; 481^2; 948^2];
+
+A = [fi0'*fi0 fi1'*fi0 fi2'*fi0; fi0'*fi1 fi1'*fi1 fi2'*fi1; fi0'*fi2 fi1'*fi2 fi2'*fi2]; #matriz de sist. de ecuaciones normales
+b = [Cota'*fi0; Cota'*fi1; Cota'*fi2]; #vector independiente del sist. de ecuaciones normales
+
+c = inv(A)*b; #vector de coeficientes de la funcion de ajuste
+
+function ajuste = a(x,c)
+    ajuste = c(1) + c(2).*x + c(3).*x.^2;
+endfunction
+
+x = 0:25:1000;
+plot(x,a(x,c))
+hold on;
+scatter(Volumen, Cota,10, 'filled')
+hold off;
+xlabel('Volumen [Hm^3]','fontsize',14)
+ylabel('Cota [m]','fontsize',14)
+title('Ajuste cuadratico','fontsize',14,'color','blue')
+grid

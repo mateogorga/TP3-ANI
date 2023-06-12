1cf6d05a0d66785e0a7624df4457de85de3284a7
mateogorga <86436097+mateogorga@users.noreply.github.com>
2023-06-12 20:40:02 -0300

    Update euler_general.m

--- euler_general.m
+++ euler_general.m
@@ -2,20 +2,40 @@
 % du1/dt = u2
 % du2/dt = -u1
 
-x0 = [1,3];     % Vector de valores iniciales de variables dependientes
+x0 = [2,20];     % Vector de valores iniciales de variables dependientes
 ne = 2;             % numero de EDO (cuantas ecuaciones diferenciales voy a resolver)
-t0 = 0;             % Valor inicial del tiempo
-tf = 5;             % Valor final del tiempo
+t0 = 0;             % Valor inicial del tiempo en dias
+tf = 365;             % Valor final del tiempo en dias
 
-dt = 1e-3;          % Tama?o de paso en el tiempo
+
+dt = 1;          % Tama?o de paso en el tiempo
 n = (tf-t0)/dt;     % Numero de iteraciones
 
-% Inicializaci髇 del tiempo
+% Inicializaci贸n del tiempo
 t(1) = t0;
 
+function [fx] = edos(x)
+%Defino mis datos y las ecuaciones diferenciales
+OD = x(1);
+DBO = x(2);
+V = 700;         % Volumen estimado en la parte b)
+Qe = 20;         % Caudal de entrada parte b)
+Qs = 25;         % Caudal de salida parte b) 
+Ka = 0.01;       % coeficiente de reaireacion
+ODs = 9;         % concentracion de oxigeno de saturacion
+Kbdo = 0.1;      % coeficiente de biodegradaci贸n m谩ximo
+Ko2 = 1.4;       % constante de semisaturacion del ox铆geno elevada al cuadrado
+
+dx_dt(1) = (((Qe*OD - Qs*OD)/V) + (Ka*(ODs - OD)) - (Kbdo*((OD^2)/((OD^2)+(Ko2))))*DBO);
+dx_dt(2) = (((Qe*DBO - Qs*OD)/V) + (Ka*(ODs - OD)) - (Kbdo*((OD^2)/((OD^2)+(Ko2))))*DBO);
+
+fx = dx_dt;
+
+end
+
 % Inicializacion de matriz x(i,j)
-% i, denota el numero de la ecuaci髇 diferencial
-% j, denota el numero de interaci髇 del metodo de Euler
+% i, denota el numero de la ecuaci贸n diferencial
+% j, denota el numero de interaci贸n del metodo de Euler
 for i = 1:ne
     x(i,1) = x0(i);
 end
@@ -32,16 +52,4 @@ end
 % Grafica tiempo vs xi
 for i = 1:ne
     subplot(ne,1,i), plot(t,x(i,:))
-end
-
-function [fx] = edos(x)
-%Defino mis datos y las ecuaciones diferenciales
-u1 = x(1);
-u2 = x(2);
-
-dx_dt(1) = u2;
-dx_dt(2) = -u1;
-
-fx = dx_dt;
-
 end
